e1b239873181fa1656b664ac11f1710d899a13ea
skconan <tulconan@gmail.com>
2016-06-05 17:09:45 +0700

    add aicontrol

--- AIControl.py
+++ AIControl.py
@@ -2,8 +2,8 @@ import rospy
 import math
 import tf
 from geometry_msgs.msg import Twist,TwistStamped,Pose,PoseStamped
-from nav_msgs.msg import Odometrt
-
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64
 class AIControl():
 
     _instance = None
@@ -13,11 +13,12 @@ class AIControl():
         return cls._instance
 
     def __init__ (self):
+        self.err = 0.5
         self.pose = Pose()
         self.auv = [0,0,0,0,0,0]
         self.goal = [-999,-999,-999]
         self.command = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
-        rospy.Subscriber ('/auv/state', Odometry, self.set_position)
+        rospy.Subscriber ('/zeabus/state', Odometry, self.set_position)
         self.turn_yaw_rel = rospy.Publisher('/fix/rel/yaw', Float64, queue_size = 10)
         self.turn_yaw_abs = rospy.Publisher('/fix/abs/yaw', Float64, queue_size = 10)
         self.depth = rospy.Publisher('/fix/abs/depth', Float64, queue_size = 10)
@@ -34,10 +35,10 @@ class AIControl():
         return temp
 
     def pub (self, tw):
-        print "linear  x:%f y:%f z:%f"%(tw.twist.linear.x,tw.twist.linear.y,tw.twist.linear.z)
-        print "angular x:%f y:%f z:%f"%(tw.twist.angular.x,tw.twist.angular.y,tw.twist.angular.z)
+        print "linear  x:%f y:%f z:%f"%(tw.linear.x,tw.linear.y,tw.linear.z)
+        print "angular x:%f y:%f z:%f"%(tw.angular.x,tw.angular.y,tw.angular.z)
         for i in xrange(3):
-            self.this.publish(tw)
+            self.command.publish(tw)
             rospy.sleep(0.05)
 
     def set_position (self, data):
@@ -61,7 +62,7 @@ class AIControl():
         self.pub(self.listToTwist([0,0,0,0,0,0]))
 
     def turn_yaw_relative (self, degree):
-        rad = math.radians (degree)
+        rad = math.radians(degree)
         rad = Float64(rad)
         for i in xrange(3):
             print rad
@@ -79,10 +80,10 @@ class AIControl():
         print 'turn_yaw_absolute'
 
     def drive_z (self,z):
-        z = Float64(z)
+        zz = Float64(z)
         for i in xrange(3):
-            print z
-            self.depth.publish(z)
+            print zz
+            self.depth.publish(zz)
             rospy.sleep(0.2)
 
         while not rospy.is_shutdown() and not(self.auv[2] >= (z-self.err) and self.auv[2] <= (z+self.err)):
@@ -92,6 +93,7 @@ class AIControl():
         for i in xrange(3):
             self.depth.publish(self.auv[2])
             rospy.sleep(0.2)
+        print 'drive_z complete'
     ##### end move auv #####
 
     ##### Navigation function #####
@@ -107,7 +109,7 @@ class AIControl():
         if(diff>=0):
             return abs(diff)
         return -abs(diff)
-        
+
     def drive_xy(self,x,y):
         ### v : velocity
         v=[0,0,0,0,0,0]
@@ -162,10 +164,6 @@ class AIControl():
         self.drive_xy(x,y)
     ##### endNavigation function #####
 
-    ##### image function #####
-    def find_object (self, object, req):
-        #return True or False
-
     def is_center (self, point, xmin, xmax, ymin, ymax):
         if (xmin <= point[0] and point[0] <= xmax) and (ymin <= point[1] and point[1] <= ymax):
             return True
@@ -179,8 +177,8 @@ class AIControl():
             if value > m_min : return m_min
             if value < m_max : return m_max
         return value
-    ##### end image function #####
+
 if __name__=='__main__':
     print 'AIControl'
-    rospy.init_node('ai_control', anonymous=True) #comment if run main.py
+    # rospy.init_node('ai_control', anonymous=True) #comment if run main.py
     aicontrol=AIControl()
